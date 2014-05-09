3ffebc9c635c70fe591435487770ec766da3b3b2
st4hoo <stasiak.pawel@gmail.com>
2014-05-09 14:40:50 +0200

    update & commit test

--- Form1.cs
+++ Form1.cs
@@ -18,25 +18,25 @@ namespace RadTreeViewExample
 
             radTreeView1.SelectedNodeChanged += radTreeView1_SelectedNodeChanged;
             radTreeView1.MultiSelect = true;
-            RadTreeNode node1 = new RadTreeNode("Inbox");
-            node1.Nodes.Add( new RadTreeNode{ Text = "i1"});
-            node1.Nodes.Add(new RadTreeNode { Text = "i2" });
-            RadTreeNode node2 = new RadTreeNode("Deleted Items");
-            RadTreeNode node3 = new RadTreeNode("Outbox");
-            RadTreeNode node4 = new RadTreeNode("Sent");
+            RadTreeNode node1 = new RadTreeNode("a");
+            node1.Nodes.Add(new RadTreeNode{ Text = "a1" });
+            node1.Nodes.Add(new RadTreeNode{ Text = "a2" });
+            RadTreeNode node2 = new RadTreeNode("b");
+            node2.Nodes.Add(new RadTreeNode { Text = "b1" });
+            node2.Nodes.Add(new RadTreeNode { Text = "b2" });
+            RadTreeNode node3 = new RadTreeNode("c");
+            RadTreeNode node4 = new RadTreeNode("d");
             radTreeView1.Nodes.Add(node1);
             radTreeView1.Nodes.Add(node2);
             radTreeView1.Nodes.Add(node3);
             radTreeView1.Nodes.Add(node4);
-            //node3.Selected = true;
-            //node4.Selected = true;
         }
 
         private void radTreeView1_SelectedNodeChanged(object sender, RadTreeViewEventArgs e)
         {
             for (int w = 0; w < radTreeView1.SelectedNode.Nodes.Count; w++)
             {
-                    radTreeView1.SelectedNode.Nodes[w].Selected = true;
+                radTreeView1.SelectedNode.Nodes[w].Selected = true;
             }
         }
     }
