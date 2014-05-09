2fd1bc32d652c8c7499f00da078b60dacdfc8ff4
st4hoo <stasiak.pawel@gmail.com>
2014-05-09 11:51:06 +0200

    example

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace RadTreeViewExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radTreeView1 = new Telerik.WinControls.UI.RadTreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.radTreeView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radTreeView1
+            // 
+            this.radTreeView1.Location = new System.Drawing.Point(12, 12);
+            this.radTreeView1.Name = "radTreeView1";
+            this.radTreeView1.Size = new System.Drawing.Size(406, 250);
+            this.radTreeView1.SpacingBetweenNodes = -1;
+            this.radTreeView1.TabIndex = 0;
+            this.radTreeView1.Text = "radTreeView1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 292);
+            this.Controls.Add(this.radTreeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.radTreeView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadTreeView radTreeView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+
+namespace RadTreeViewExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            radTreeView1.SelectedNodeChanged += radTreeView1_SelectedNodeChanged;
+            radTreeView1.MultiSelect = true;
+            RadTreeNode node1 = new RadTreeNode("Inbox");
+            node1.Nodes.Add( new RadTreeNode{ Text = "i1"});
+            node1.Nodes.Add(new RadTreeNode { Text = "i2" });
+            RadTreeNode node2 = new RadTreeNode("Deleted Items");
+            RadTreeNode node3 = new RadTreeNode("Outbox");
+            RadTreeNode node4 = new RadTreeNode("Sent");
+            radTreeView1.Nodes.Add(node1);
+            radTreeView1.Nodes.Add(node2);
+            radTreeView1.Nodes.Add(node3);
+            radTreeView1.Nodes.Add(node4);
+            //node3.Selected = true;
+            //node4.Selected = true;
+        }
+
+        private void radTreeView1_SelectedNodeChanged(object sender, RadTreeViewEventArgs e)
+        {
+            for (int w = 0; w < radTreeView1.SelectedNode.Nodes.Count; w++)
+            {
+                    radTreeView1.SelectedNode.Nodes[w].Selected = true;
+            }
+        }
+    }
+}
